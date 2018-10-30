19c928fabc7f4278ec85b18f89042ba65146386f
gotwasin <34157610+gotwasin@users.noreply.github.com>
2018-10-30 17:36:03 +0700

    Update insert.sh

--- insert.sh
+++ insert.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 token=$(kubeadm token list | awk '{print $1}' | sed -n 2p)
 discoveryToken=$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')
 ipmaster=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
